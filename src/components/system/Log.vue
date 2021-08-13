b1a421a7bb2007fe1858b2488c5d9c75bce6beb8
qichaoyang <chaoyang.qi@mybogames.com>
2021-08-13 18:30:12 +0800

    xxx

--- src/components/system/Log.vue
+++ src/components/system/Log.vue
@@ -15,9 +15,6 @@
             <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
-        <el-col :span="4">
-          <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
-        </el-col>
       </el-row>
       <!-- 用户列表区域 -->
       <el-table :data="userlist" border stripe>
@@ -74,138 +71,13 @@ export default {
     },
     // 监听 pagesize改变的事件
     handleSizeChange (newSize) {
-      // console.log(newSize)
       this.queryInfo.page_size = newSize
       this.getUserList()
     },
     // 监听 页码值 改变事件
     handleCurrentChange (newSize) {
-      // console.log(newSize)
       this.queryInfo.page_num = newSize
       this.getUserList()
-    },
-    // 监听 switch开关 状态改变
-    async userStateChanged (userInfo) {
-      this.userInfo.enabled = userInfo.enabled ? 1 :0
-      this.userInfo.userId = userInfo.userId
-      console.log( this.userInfo)
-      const { data: res } = await this.$http.post('/user/updateStatus', this.userInfo)
-      if (res.code !== 200) {
-        userInfo.enabled = !userInfo.enabled
-        return this.$message.error('更新用户状态失败')
-      }
-      this.$message.success('更新用户状态成功！')
-    },
-    // 监听 添加用户对话框的关闭事件
-    addDialogClosed () {
-      this.$refs.addUserFormRef.resetFields()
-    },
-    // 添加用户
-    addUser () {
-      // 提交请求前，表单预验证
-      this.$refs.addUserFormRef.validate(async valid => {
-        // console.log(valid)
-        // 表单预校验失败
-        if (!valid) return
-        const { data: res } = await this.$http.post('/user/saveUser', this.addUserForm)
-        if (res.code !== 200) {
-          this.$message.error('添加用户失败！')
-        }
-        this.$message.success('添加用户成功！')
-        // 隐藏添加用户对话框
-        this.addDialogVisible = false
-        this.getUserList()
-      })
-    },
-    // 编辑用户信息
-    async showEditDialog (userInfo) {
-      this.editUserForm = userInfo
-      this.editUserForm.userId = userInfo.userId
-      this.editDialogVisible = true
-    },
-    // 监听修改用户对话框的关闭事件
-    editDialogClosed () {
-      this.$refs.editUserFormRef.resetFields()
-    },
-    // 修改用户信息
-    editUser () {
-      // 提交请求前，表单预验证
-      this.$refs.editUserFormRef.validate(async valid => {
-        // 表单预校验失败
-        if (!valid) return
-        const { data: res } = await this.$http.post(
-          '/user/update',
-          {
-            password: this.editUserForm.password,
-            userId  :   this.editUserForm.userId
-          }
-        )
-        if (res.code !== 200) {
-          this.$message.error(res.msg)
-        }
-        // 隐藏添加用户对话框
-        this.editDialogVisible = false
-        this.$message.success('更新用户信息成功！')
-        this.getUserList()
-      })
-    },
-    // 删除用户
-    async removeUserById (id) {
-      const confirmResult = await this.$confirm(
-        '此操作将永久删除该用户, 是否继续?',
-        '提示',
-        {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }
-      ).catch(err => err)
-      // 点击确定 返回值为：confirm
-      // 点击取消 返回值为： cancel
-      if (confirmResult !== 'confirm') {
-        return this.$message.info('已取消删除')
-      }
-      const { data: res } = await this.$http.post('/user/deleteById',
-              {"userId" :id })
-      if (res.code !== 200) return this.$message.error('删除用户失败！')
-      this.$message.success('删除用户成功！')
-      this.getUserList()
-    },
-    // 展示分配角色的对话框
-    async showSetRole (role) {
-      console.log(role)
-      // <p>当前用户：{{userInfo.username}}</p>
-      //<p>当前角色：{{userInfo.role_name}}</p>
-      this.userInfo.username = role.userName
-      this.userInfo.role_name = role.Role.role_name
-      this.userInfo.user_id = role.userId
-      // 展示对话框之前，获取所有角色列表
-      const { data: res } = await this.$http.post('/role/all')
-      if (res.code !== 200) {
-        return this.$message.error('获取角色列表失败！')
-      }
-      this.rolesLsit = res.data
-      this.setRoleDialogVisible = true
-    },
-    // 分配角色
-    async saveRoleInfo () {
-      if (!this.selectRoleId) {
-        return this.$message.error('请选择要分配的角色')
-      }
-      const { data: res } = await this.$http.post('/user/insertRole',{ role_id: this.selectRoleId,
-        user_id : this.userInfo.user_id
-      })
-      if (res.code !== 200) {
-        return this.$message.error(res.msg)
-      }
-      this.$message.success('更新角色成功！')
-      this.getUserList()
-      this.setRoleDialogVisible = false
-    },
-    // 分配角色对话框关闭事件
-    setRoleDialogClosed () {
-      this.selectRoleId = ''
-      this.userInfo = {}
     }
   }
 }
